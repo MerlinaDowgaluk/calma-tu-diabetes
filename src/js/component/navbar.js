d411f86f57b3fe0323cee2bf0b804228afedad24
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2024-04-26 08:12:18 +0000

    navbar and fonts

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import calma from "../../img/calma.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-light mb-3 container-fluid" style={{backgroundColor: "rgb(251, 242, 249)"}}>
+			<div className="d-flex col-12">
+				<div className="col-4 mt-4">
+					<Link to="/" className="text-decoration-none">
+						<span className="navbar-brand h1 ms-1" style={{color: "#714994", fontStyle: "italic", fontWeight: "700"}}>CALMA tu diabetes</span>
+					</Link>
+				</div>
+				<div className="ml-auto justify-content-center d-flex col-4">
+					<img className="rounded" src={calma} style={{maxWidth: "20%"}}></img>
+				</div>
+				<div className="col-4">
+
+				</div>
 			</div>
 		</nav>
 	);
