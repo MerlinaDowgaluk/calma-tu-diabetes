0ebc25ee07faaaec376d4ffb7df3bd58d7b25b19
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2024-04-29 19:12:24 +0000

    add home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,12 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import homeimg from "../../img/home.png"
 
 export const Home = () => (
-	<div className="text-center mt-5 d-flex" style={{color: "#714994"}}>
-		<h1>Te acompaño a CAMBIAR tu relación con la diabetes + allá de los números</h1>
+	<div className="text-center mt-5 mb-5 d-flex" style={{color: "#714994"}}>
+		<h1 className="col-6 mt-5 ms-5 text-start">Te acompaño a CAMBIAR tu relación con la diabetes + allá de los números</h1>
+		<div className="col-5">
+			<img className="rounded" src={homeimg} style={{width: "70%"}}></img>
+		</div>
 	</div>
 );
